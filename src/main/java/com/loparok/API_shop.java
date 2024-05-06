8d06413849b49d9a80f184f52d8a70defb1fc692
depiahutas <barbey.valentin1@gmail.com>
2024-05-06 11:25:40 +0200

    first commit

---
+++ src/main/java/com/loparok/API_shop.java
@@ -0,0 +1,13 @@
+package com.loparok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class API_shop {
+
+	public static void main(String[] args) {
+		SpringApplication.run(API_shop.class, args);
+	}
+
+}
